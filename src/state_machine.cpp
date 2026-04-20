17a5b3916e9c7dd2a0e8208913c821a89a58a2b4
kfowl3r <kylafowl3r@gmail.com>
2026-04-20 12:40:23 -0400

    e

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,74 +1,162 @@
 #include <Arduino.h>
 #include <Pixy2.h>
-#include "PID_motors.h"
 
+extern Pixy2 pixy;
+
+extern double setpoint;
+extern int baseSpeed;
+
+void PID_control();
+void stopMotors();
+void resetPIDHeading();
+
+// Pixy tracking constants
 const int SIG_ORANGE = 1;
-const int camCenter_X = 158;
+const int CAM_CENTER_X = 158;
+const int CENTER_TOL = 8;
 
-const int SEARCH_SPEED = 25;
-const int TURN_STEP = 6;
-const int CENTER_TOL = 15;
+// tune these on the robot
+const int FAST_SPEED = 165;
+const int SLOW_SPEED = 120;
+const float TURN_GAIN = 0.09;      // deg of heading change per pixel error
+const float MAX_TURN_DELTA = 10.0; // limit heading jump per loop
+const int CLOSE_AREA = 5200;       // stop / slow when puck is very close
+const unsigned long LOST_TIMEOUT = 250;
 
+// enum states
+enum State {
+  SEARCH_PUCK,FOLLOW_PUCK
+};
+
+State state = SEARCH_PUCK;
+
+// ===== tracking variables =====
 bool orangeSeen = false;
-int orangeX = 0;
+int orangeX = CAM_CENTER_X;
+int orangeArea = 0;
+unsigned long lastSeenTime = 0;
+unsigned long lastSearchUpdate = 0;
+bool searchDirRight = true;
 
-Motor motors;
-Pixy2 pixy;
-
-enum State {IDLE, CHASE_PUCK};
-State state = IDLE;
-
+// functions
 bool detectOrange();
 void updateStateMachine();
-void stateIDLE();
-void stateChasePuck();
+void stateSearchPuck();
+void stateFollowPuck();
+float wrapAngle(float a);
 
+float wrapAngle(float a) {
+  while (a >= 360.0) a -= 360.0;
+  while (a < 0.0) a += 360.0;
+  return a;
+}
+
+// detecting orange
 bool detectOrange() {
   pixy.ccc.getBlocks();
   orangeSeen = false;
 
+  int bestIndex = -1;
+  int bestArea = 0;
+
   for (int i = 0; i < pixy.ccc.numBlocks; i++) {
     if (pixy.ccc.blocks[i].m_signature == SIG_ORANGE) {
-      orangeSeen = true;
-      orangeX = pixy.ccc.blocks[i].m_x;
-      return true;
+      int area = pixy.ccc.blocks[i].m_width * pixy.ccc.blocks[i].m_height;
+
+      // track biggest orange object
+      if (area > bestArea) {
+        bestArea = area;
+        bestIndex = i;
+      }
     }
   }
+
+  if (bestIndex >= 0) {
+    orangeSeen = true;
+    orangeX = pixy.ccc.blocks[bestIndex].m_x;
+    orangeArea = bestArea;
+    lastSeenTime = millis();
+    return true;
+  }
+
+  orangeArea = 0;
   return false;
 }
 
+// switches
 void updateStateMachine() {
+  detectOrange();
+
   switch (state) {
-    case IDLE:
-      stateIDLE();
+    case SEARCH_PUCK:
+      stateSearchPuck();
       break;
-    case CHASE_PUCK:
-      stateChasePuck();
+
+    case FOLLOW_PUCK:
+      stateFollowPuck();
       break;
   }
 }
 
-void stateIDLE() {
-  motors.stopMotors();
-}
-
-void stateChasePuck() {
-  if (!detectOrange()) {
-    motors.update(SEARCH_SPEED);
-    motors.update(SEARCH_SPEED);
+void stateSearchPuck() {
+  if (orangeSeen) {
+    state = FOLLOW_PUCK;
     return;
   }
 
-  if (orangeX < camCenter_X - CENTER_TOL) {
-    motors.setTurn(-TURN_STEP);
-  }
-  else if (orangeX > camCenter_X + CENTER_TOL) {
-    motors.setTurn(+TURN_STEP);
-  }
+  // keep moving slowly while sweeping heading
+  baseSpeed = SLOW_SPEED;
 
-  if (setpoint >= 360.0) setpoint -= 360.0;
-  if (setpoint < 0.0) setpoint += 360.0;
+  if (millis() - lastSearchUpdate > 90) {
+    if (searchDirRight) {
+      setpoint += 8.0;
+    } else {
+      setpoint -= 8.0;
+    }
+
+    setpoint = wrapAngle(setpoint);
+    searchDirRight = !searchDirRight;
+    lastSearchUpdate = millis();
+  }
 
   PID_control();
-  delay(20);
+}
+
+void stateFollowPuck() {
+  if (!orangeSeen) {
+    if (millis() - lastSeenTime > LOST_TIMEOUT) {
+      state = SEARCH_PUCK;
+    }
+    PID_control();
+    return;
+  }
+
+  int pixelError = orangeX - CAM_CENTER_X;
+
+  // speed logic:
+  // go faster when puck is farther away, slower when very close
+  if (orangeArea > CLOSE_AREA) {
+    baseSpeed = 90;
+  } else {
+    baseSpeed = FAST_SPEED;
+  }
+
+  // if nearly centered, drive mostly straight
+  if (abs(pixelError) <= CENTER_TOL) {
+    PID_control();
+    return;
+  }
+
+  // convert camera error into heading correction
+  float turnDelta = TURN_GAIN * pixelError;
+
+  if (turnDelta > MAX_TURN_DELTA) turnDelta = MAX_TURN_DELTA;
+  if (turnDelta < -MAX_TURN_DELTA) turnDelta = -MAX_TURN_DELTA;
+
+  // IMPORTANT:
+  // update setpoint every loop so the robot keeps tracking a moving puck
+  setpoint += turnDelta;
+  setpoint = wrapAngle(setpoint);
+
+  PID_control();
 }
