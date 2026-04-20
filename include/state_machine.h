78bf7f5ee191c132da4d9d4a41a020543b0e3cf6
kfowl3r <kylafowl3r@gmail.com>
2026-04-19 20:20:20 -0400

    push iit

---
+++ include/state_machine.h
@@ -0,0 +1,40 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <Arduino.h>
+#include <Adafruit_BNO055.h>
+#include <DualMAX14870MotorShield.h>
+
+class Motor {
+private:
+    DualMAX14870MotorShield motors;
+    Adafruit_BNO055 bno;
+
+    // PID variables
+    double setpoint;
+    double Kp, Ki, Kd;
+    double error, previousError, integral, derivative;
+    unsigned long previousTime;
+    int baseSpeed;
+
+    // Turning
+    double turnGoalYaw;
+    bool turning;
+    unsigned long turnStartTime;
+
+    // Helpers
+    double getYaw();
+    double angleDiff(double target, double current);
+    void PID_control();
+    void resetPIDHeading();
+
+public:
+    Motor();
+
+    void initMotors();
+    void setTurn(double targetYaw);
+    void stopMotors();
+    void update();
+};
+
+#endif
